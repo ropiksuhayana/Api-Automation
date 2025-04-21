9af0b4d2d4dc9c426c713eecefea734305ae902c
ropik suhayana <ropiksuhayana@gmail.com>
2025-04-21 23:01:58 +0700

    api automation with newman

---
+++ newman-script/run-newman.js
@@ -0,0 +1,22 @@
+
+const newman = require("newman");
+
+newman.run({
+    // collection: "json-collection/rofiq.postman_collection.json",
+    collection: require('../json-collection/rofiq_new.postman_collection.json'),
+    environment: require('../json-env/rofiq-env.json'),
+    reporters: ["cli", "htmlextra"],
+    reporter: {
+        htmlextra: {
+            export: "./report/report.html", // path untuk hasil report
+            title: "API Automation Report"
+        }
+    }
+}, function (err, summary) {
+    if (err) {
+        console.error("Terjadi kesalahan saat menjalankan Newman:", err);
+        process.exit(1);
+    }
+    console.log("Newman test selesai.");
+});
+
